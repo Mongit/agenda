feb8e8f187af17a73f191176fb2b31208b9aa99e
Mon se <monserrat.jimenez2@gmail.com>
2015-10-09 09:44:44 -0500

    delete comments

--- agenda/routes/index.js
+++ agenda/routes/index.js
@@ -1,24 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-/*    require.config({
-        paths: {
-            "moment": "./../../public/fullcalendar/lib/moment.min.js"
-        },
-        config: {
-            moment: {
-                noGlobal: true
-            }
-        }
+    res.render('index', { 
+        title: 'My Server side event',
+        start: moment("2015-10-15T19:32:21.196Z"),
+        end: moment("2015 10 03", "YYYY MM DD"),
+        allDay: false,
+        editable: true,//permite el movimiento del evento
+        color: 'yellow',   // an option!
+        textColor: 'black' // an option!
     });
-
-    define(["moment"], function(moment){
-        moment().format;
-    });
-*/
-    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
