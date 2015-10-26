46cdb5cf4b0fe5addc198d8e5d3231eda825da66
Mon se <monserrat.jimenez2@gmail.com>
2015-10-26 08:01:02 -0600

    revisar cambios de eje22

--- cuadroDialogo/public/ejemploJQuery/eje22.js
+++ cuadroDialogo/public/ejemploJQuery/eje22.js
@@ -1,70 +1,30 @@
 $(document).ready(function() {
     $(function() {
-        $("#dialog").dialog({
-            autoOpen: false
-        });
-        $("#button").on("click", function() {
+        var cancel = function() {
+            $("dialog").dialog("close");
+        }
+        
+        var getResponse = function() {
+            var answer;
+            $("input").each(function() {
+                console.log(this);
+                (this.checked === true || this !== "") ? answer = $(this).val() : null;
+            $("<p>").text(answer).insertAfter($("#button"));
+            });
+            $("#dialog").dialog("close");
+        }
+        
+        var dialogOpts = {
+            //modal: true,
+            buttons: {
+                "Save": getResponse,
+                "Cancel": cancel
+            },
+            autoOpen:false
+        };
+        $("#dialog").dialog(dialogOpts);
+        $("#button").click(function() {
             $("#dialog").dialog("open");
         });
     });
-    $("#cancel").click(function() {
-        $("#dialog").dialog("close");
-    });
-    
-    // Validating Form Fields.....
-    $("#submit").click(function(e) {
-        var answer;
-            
-            $("input").each(function() {
-                (this !== null) ?  answer = $(this).val : null;
-            });            
-            $("<p>").text("Your answer: " + answer).insertAfter($("dialog"));
-            $("#dialog").dialog("close");
-        /*
-        var eventName = $("#title").val();
-        if (eventName ==! '') {
-            alert(eventName);
-            e.preventDefault();
-        }
-        
-        var name = $("#name").val();
-        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        if (email === '' || name === '') {
-            alert("Please fill all fields...!!!!!!");
-            e.preventDefault();
-        } else if (!(email).match(emailReg)) {
-            alert("Invalid Email...!!!!!!");
-            e.preventDefault();
-        } else {
-            alert("Form Submitted Successfully......");
-        }*/
-    });
-    /*
-    $(function() {
-        var execute = function(){
-            var answer;
-            
-            $("input").each(function() {
-                (this.checked === true) ? answer = $(this).val() : null;
-            });
-            
-            $("<p>").text("Your answer: " + answer).appendTo($("body"));
-            $("#dialog").dialog("close");
-        };
-
-        var cancel = function() {
-            $("#dialog").dialog("close");
-        }
-    
-    
-        var dialogOpts = {
-            buttons: {
-                "Submit": execute,
-                "Cancel": cancel
-            }
-        };
-        $("#dialog").dialog(dialogOpts);    
-    });
-    */
-    
 });
